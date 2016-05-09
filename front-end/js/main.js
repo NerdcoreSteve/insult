b0d925cff777cf07ab9e8e65db286e0ac09fb66a
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2016-05-08 21:52:26 -0600

    insult generator looking good, grammar not quite right. added node modules to repo

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -1,2 +1,59 @@
-console.log([1, 2, 3].map((x) => 2 * x))
-console.log(R)
+requirejs([
+    'node_modules/ramda/dist/ramda'
+], function (R) {
+    //Impure helpers
+    var rand =
+        R.curry((min, max) => Math.round(Math.random() * (max - min) + min))
+
+    var tap = (x) => {
+        console.log(x)
+        return x
+    }
+
+    //Pure code
+    var property_list_length = R.curry((object, property) =>
+        R.compose(
+            R.length,
+            R.prop(property))
+                (object))
+
+    var insult = function (insult_data, insult_object) {
+        var subject = insult_data.subjects[insult_object.subject],
+            object = insult_data.objects[insult_object.object],
+            verb = insult_data.verbs[insult_object.verb]
+        return ` ${subject} ${verb} ${object}`
+    }
+
+    //Impure app code
+    var insult_data = {
+        subjects: [
+            'You',
+            'Your mother',
+            'Your father',
+        ],
+        verbs: [
+            'smells like',
+            'is a',
+            'consorts with',
+        ],
+        objects: [
+            'a hamster',
+            'elderberries'
+        ]
+    }
+
+    //TODO I can make this pure by returning a function that is impure
+    var rand_insult_data_el = (property) =>
+        rand(0, property_list_length(insult_data, property) - 1)
+
+    //TODO keyboard bindings
+    document.querySelector('.insult-button').onclick = () =>
+        document.querySelector('.insult').innerHTML =
+            insult(
+                insult_data,
+                {
+                    subject: rand_insult_data_el('subjects'),
+                    verb: rand_insult_data_el('verbs'),
+                    object: rand_insult_data_el('objects')
+                })
+})
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,45 @@
-"use strict";
+'use strict';
 
-console.log([1, 2, 3].map(function (x) {
-  return 2 * x;
-}));
-console.log(R);
+requirejs(['node_modules/ramda/dist/ramda'], function (R) {
+    //Impure helpers
+    var rand = R.curry(function (min, max) {
+        return Math.round(Math.random() * (max - min) + min);
+    });
+
+    var tap = function tap(x) {
+        console.log(x);
+        return x;
+    };
+
+    //Pure code
+    var property_list_length = R.curry(function (object, property) {
+        return R.compose(R.length, R.prop(property))(object);
+    });
+
+    var insult = function insult(insult_data, insult_object) {
+        var subject = insult_data.subjects[insult_object.subject],
+            object = insult_data.objects[insult_object.object],
+            verb = insult_data.verbs[insult_object.verb];
+        return ' ' + subject + ' ' + verb + ' ' + object;
+    };
+
+    //Impure app code
+    var insult_data = {
+        subjects: ['You', 'Your mother', 'Your father'],
+        verbs: ['smells like', 'is a', 'consorts with'],
+        objects: ['a hamster', 'elderberries']
+    };
+
+    //TODO I can make this pure by returning a function that is impure
+    var rand_insult_data_el = function rand_insult_data_el(property) {
+        return rand(0, property_list_length(insult_data, property) - 1);
+    };
+
+    document.querySelector('.insult-button').onclick = function () {
+        return document.querySelector('.insult').innerHTML = insult(insult_data, {
+            subject: rand_insult_data_el('subjects'),
+            verb: rand_insult_data_el('verbs'),
+            object: rand_insult_data_el('objects')
+        });
+    };
+});
