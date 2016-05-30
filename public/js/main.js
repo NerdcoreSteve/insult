e52f2c4d10eaef5a2e598c1fb581585bdd7fb37b
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2016-05-30 13:41:30 -0600

    added more insult data

--- public/js/main.js
+++ public/js/main.js
@@ -33,6 +33,54 @@ module.exports = {
     }, {
         part: 'Your nose',
         first_person: false
+    }, {
+        part: 'Your mongoose',
+        first_person: false
+    }, {
+        part: 'Your secret goblin friend',
+        first_person: false
+    }, {
+        part: 'The fluffy pink teddy bear of your heart',
+        first_person: false
+    }, {
+        part: 'Your ballroom blitz dance partner',
+        first_person: false
+    }, {
+        part: 'Your beehive',
+        first_person: false
+    }, {
+        part: 'Your basement gnome',
+        first_person: false
+    }, {
+        part: 'Your robot butler',
+        first_person: false
+    }, {
+        part: 'Your brother',
+        first_person: false
+    }, {
+        part: 'Your sister',
+        first_person: false
+    }, {
+        part: "Your uncle's cousin's brother's hamster",
+        first_person: false
+    }, {
+        part: "Your turtle's best friend",
+        first_person: false
+    }, {
+        part: 'Your dog',
+        first_person: false
+    }, {
+        part: 'Your cat',
+        first_person: false
+    }, {
+        part: 'Your car',
+        first_person: false
+    }, {
+        part: 'The back of your head',
+        first_person: false
+    }, {
+        part: 'Your bookshelf',
+        first_person: false
     }],
     verbs: [{
         first_person: 'smell like',
@@ -45,6 +93,34 @@ module.exports = {
     }, {
         first_person: 'associate with',
         third_person: 'associates with',
+        plural: true
+    }, {
+        first_person: 'sniff',
+        third_person: 'sniffs',
+        plural: true
+    }, {
+        first_person: 'go to tea parties with',
+        third_person: 'goes to tea parties with',
+        plural: true
+    }, {
+        first_person: 'hug',
+        third_person: 'hugs',
+        plural: true
+    }, {
+        first_person: 'have nights on the town with',
+        third_person: 'has nights on the town with',
+        plural: true
+    }, {
+        first_person: 'have a pimple on your nose that looks like',
+        third_person: 'has a pimple on their nose that looks like',
+        plural: false
+    }, {
+        first_person: 'have a pimple on your nose that looks like',
+        third_person: 'has a pimple on their nose that looks like',
+        plural: false
+    }, {
+        first_person: 'often have dinner with',
+        third_person: 'often has dinner with',
         plural: true
     }],
     objects: [{
